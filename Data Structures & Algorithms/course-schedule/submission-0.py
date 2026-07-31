588627002add48f5166ce6dd2a3fa9ffe9eff6ff
BrianChengEn <102721517+BrianChengEn@users.noreply.github.com>
2026-07-31 19:51:27 -0400

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prereq = [[] for _ in range(numCourses)]
+
+        # 0 = haven't check
+        # 1 = checking
+        # 2 = don't have loop
+        state = [0] * numCourses
+
+        for course, pre in prerequisites:
+            prereq[course].append(pre)
+
+        def dfs(course):
+            if state[course] == 1:
+                return False
+            if state[course] == 2:
+                return True
+            
+            state[course] = 1
+
+            for pre in prereq[course]:
+                if not dfs(pre):
+                    return False
+            
+            state[course] = 2
+            return True
+
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        
+        return True
